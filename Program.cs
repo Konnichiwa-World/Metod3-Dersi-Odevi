4190c6a75c0c160eb76a03f2355d5407bf7db792
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-04-27 15:50:22 +0300

    Metod3 Odevi
    ...

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dizi = "Patika".ConvertToChars();
+
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static char[] ConvertToChars(this string text)
+        {
+            char[] chars = new char[text.Length];
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = text[i];
+            }
+            return chars;
+        }
+    }
+}
